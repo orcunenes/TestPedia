b5324e3a67d0c3b978ead6c579cf0551ce556b0d
orcunenes <vvalentos.1@gmail.com>
2023-02-19 15:03:22 +0300

    missing files added

---
+++ Testpedia/Connect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data.SqlClient;
+
+namespace Testpedia
+{
+    public class Connect
+    {
+       public static SQLiteConnection connection = new SQLiteConnection("Data Source=C:\\Users\\orcue\\source\\repos\\Testpedia\\TestPediaDB.db;Version=3;");
+    }
+}
---
+++ Testpedia/Fetch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testpedia
+{
+    public class Fetch
+    {
+       static DataTable dt;
+        public static DataTable Getdata(string sql)
+        {
+            dt =new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(sql,Connect.connection);
+            adapter.Fill(dt);
+            return dt;
+        }
+    }
+}
